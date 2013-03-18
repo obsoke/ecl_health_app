6ee5e1ebbedb91e6d6359f6a220bb72ea213fd7a
Dale Karp <me@dale.io>
2013-03-17 22:28:12 -0400

    inflating layouts in Dashboard/SignUpActivity

--- src/cs/ecl/karpAndMamidala/tawmylf/SignUpActivity.java
+++ src/cs/ecl/karpAndMamidala/tawmylf/SignUpActivity.java
@@ -13,5 +13,6 @@ import android.os.Bundle;
 public class SignUpActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.SignUpLayout);
     }
 }
