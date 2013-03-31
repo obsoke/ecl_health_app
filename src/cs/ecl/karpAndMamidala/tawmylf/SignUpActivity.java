7016ce15e03aaa28ca4d2088126c27852c779cbd
Dale Karp <me@dale.io>
2013-03-31 18:03:50 -0400

    status update

--- src/cs/ecl/karpAndMamidala/tawmylf/SignUpActivity.java
+++ src/cs/ecl/karpAndMamidala/tawmylf/SignUpActivity.java
@@ -2,6 +2,7 @@ package cs.ecl.karpAndMamidala.tawmylf;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,5 +15,11 @@ public class SignUpActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_layout);
+    }
+
+    public void signUp(View v) {
+        //TODO: get value/validate form
+        //TODO: if form is valid, create new user
+        //TODO: if not, give error message to user
     }
 }
--- src/cs/ecl/karpAndMamidala/tawmylf/WelcomeActivity.java
+++ src/cs/ecl/karpAndMamidala/tawmylf/WelcomeActivity.java
@@ -1,15 +1,18 @@
 package cs.ecl.karpAndMamidala.tawmylf;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_layout);
+    }
+
+    public void toSignUp(View v) {
+        //TODO: send user to sign up activity
     }
 }
