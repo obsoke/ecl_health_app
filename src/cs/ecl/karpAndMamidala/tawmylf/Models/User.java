863e7fddeb62558c6bbdf65efc06f22f3b865f2b
Dale Karp <me@dale.io>
2013-04-05 15:37:00 -0400

    serializing user

--- src/cs/ecl/karpAndMamidala/tawmylf/Models/User.java
+++ src/cs/ecl/karpAndMamidala/tawmylf/Models/User.java
@@ -1,6 +1,9 @@
 package cs.ecl.karpAndMamidala.tawmylf.Models;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
     private long id;
     private String name;
     private int gender;
@@ -9,6 +12,29 @@ public class User {
     private String emerg_name;
     private String emerg_address;
     private String emerg_phone;
+
+    private User(Parcel in) {
+        this.id = in.readLong();
+        this.name = in.readString();
+        this.gender = in.readInt();
+        this.address = in.readString();
+        this.phone = in.readString();
+        this.emerg_name = in.readString();
+        this.emerg_address = in.readString();
+        this.emerg_phone = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[0];
+        }
+    };
 
     public long getId() {
         return id;
@@ -77,5 +103,22 @@ public class User {
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(this.id);
+        dest.writeString(this.name);
+        dest.writeInt(this.gender);
+        dest.writeString(this.address);
+        dest.writeString(this.phone);
+        dest.writeString(this.emerg_name);
+        dest.writeString(this.emerg_address);
+        dest.writeString(this.emerg_phone);
     }
 }
