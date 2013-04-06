5f36fbbfc28e33d0ab8edc5437d421a8c76ab37e
Dale Karp <me@dale.io>
2013-04-06 00:07:49 -0400

    math helper added

--- src/cs/ecl/karpAndMamidala/liveforever/Activities/AnalyzeWeightActivity.java
+++ src/cs/ecl/karpAndMamidala/liveforever/Activities/AnalyzeWeightActivity.java
@@ -6,7 +6,12 @@ import android.widget.TableLayout;
 import cs.ecl.karpAndMamidala.liveforever.Database.WeightDataSource;
 import cs.ecl.karpAndMamidala.liveforever.Models.WeightItem;
 import cs.ecl.karpAndMamidala.liveforever.R;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+import org.apache.commons.math3.stat.descriptive.rank.Median;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class AnalyzeWeightActivity extends Activity {
@@ -21,7 +26,19 @@ public class AnalyzeWeightActivity extends Activity {
         TableLayout tableView = (TableLayout) findViewById(R.id.analyticsTable);
         // get all weight items
         List<WeightItem> itemList = dataSource.getAllWeightItems();
+        List<Float> weightList = this.getWeight(itemList);
         // create data rows
         // add rows to table
+    }
+
+
+
+    private List<Float> getWeight(List<WeightItem> wi) {
+        List<Float> rv = new ArrayList<Float>();
+        for (WeightItem item: wi) {
+            rv.add(item.getWeight());
+        }
+        Collections.sort(rv);
+        return rv;
     }
 }
---
+++ src/cs/ecl/karpAndMamidala/liveforever/Utilities/MathHelper.java
@@ -0,0 +1,93 @@
+package cs.ecl.karpAndMamidala.liveforever.Utilities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dale
+ * Date: 2013-04-05
+ * Time: 11:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MathHelper {
+    static public float getMax(List<Float> wl) {
+        return Collections.max(wl);
+    }
+
+    static public float getMin(List<Float> wl) {
+        return Collections.min(wl);
+    }
+
+    static public float getAvg(List<Float> wl) {
+        float rv = 0;
+        for (float i: wl) {
+            rv += i;
+        }
+        return rv / wl.size();
+    }
+
+    static public float getMedian(List<Float> wl) {
+        int middleIdx = wl.size() / 2;
+        if (middleIdx % 2 == 1) {
+            return wl.get(middleIdx);
+        }
+        else {
+            float rv = wl.get(middleIdx) + wl.get(middleIdx - 1) / 2;
+            return rv;
+        }
+    }
+
+    static public float getMode(List<Float> wl) {
+        float rv = 0;
+        float highestFreq = 0;
+
+        int size = wl.size();
+        for (int i = 0; i < size; ++i) {
+            int c = 0;
+            for (int x = 0; x < size; ++x) {
+                if (wl.get(i) == wl.get(x)) c++;
+            }
+            if (c > highestFreq) {
+                highestFreq = c;
+                rv = wl.get(i);
+            }
+        } // end for
+        return rv;
+    } // end getMode()
+
+    static public float getRange(List<Float> wl) {
+        float min = MathHelper.getMin(wl);
+        float max = MathHelper.getMax(wl);
+        return max - min;
+    }
+
+    static public float getVariance(List<Float> wl) {
+        float avg = MathHelper.getAvg(wl);
+
+        List<Float> subMean = new ArrayList<Float>();
+        for (float i: wl) {
+            subMean.add(i - avg);
+        }
+
+        List<Float> squared = new ArrayList<Float>();
+        for(float i: subMean) {
+            squared.add(i * i);
+        }
+
+
+        float addedSquares = 0;
+        for(float i: squared) {
+            addedSquares += i;
+        }
+        return (addedSquares / (wl.size() - 1));
+    }
+
+    static public double getStandardDeviation(List<Float> wl) {
+        float variance = MathHelper.getVariance(wl);
+        return Math.sqrt(variance);
+    }
+
+
+}
